d5b85bbcf217f82e202a8eb12713329d427dd28c
EduardoNotbook <duduredtone@gmail.com>
2024-04-01 09:20:37 -0300

    Separação em pastas

---
+++ .history/IceTalk_FrontEnd/site/folders/pedidos_20240401084012.js
@@ -0,0 +1,26 @@
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita o envio do formulário padrão
+
+    const formData = new FormData(this); // Obtém os dados do formulário
+
+    fetch('http://localhost:8080/api/funcionarios', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(Object.fromEntries(formData))
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro ao enviar dados.');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Dados enviados com sucesso:', data);
+        
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+    });
+});
---
+++ .history/IceTalk_FrontEnd/site/folders/pedidos_20240401090825.js
@@ -0,0 +1,56 @@
+document.getElementById("myForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Evita o envio do formulário padrão
+
+    const formData = new FormData(this); // Obtém os dados do formulário
+
+    fetch('http://localhost:8080/api/funcionarios', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(Object.fromEntries(formData))
+    })
+        .then(response => {
+
+            if (!response.ok) {
+                pushNotify('error', "Vazio", "escreva algo");
+                throw new Error('Erro ao enviar dados.');
+            } else {
+                pushNotify("sucess", "Sucesso", "Dados enviados com sucesso");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Dados enviados com sucesso:', data);
+
+
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        });
+
+
+
+
+});
+
+
+function pushNotify(text) {
+    new Notify({
+        status: 'success',
+        title: 'Notify Title',
+        text: text,
+        effect: 'fade',
+        speed: 300,
+        customClass: null,
+        customIcon: null,
+        showIcon: true,
+        showCloseButton: true,
+        autoclose: true,
+        autotimeout: 3000,
+        gap: 20,
+        distance: 20,
+        type: 'outline',
+        position: 'right top'
+    })
+}
